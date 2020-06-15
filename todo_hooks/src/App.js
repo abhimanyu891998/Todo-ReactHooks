2998b11046d08a6b4811aa658cfcd8c02dcc0031
Abhimanyue Singh Tanwar <singhabhimanyu186@gmail.com>
2020-06-15 23:00:01 +0530

    Todos added

---
+++ todo_hooks/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function Todo({todo , index}){
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoForm({addTodo}){
+  const [value, setValue] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+
+}
+
+function App() {
+  
+  const [todos, setTodos] = useState([
+    {
+      text : 'Learn about React hooks',
+      isCompleted: false
+    },
+    {
+      text : 'Complete bank stuff',
+      isCompleted: false
+    },
+    {
+      text : 'Build cool apps',
+      isCompleted: false
+    },
+    {
+      text : 'Catch up on peeps',
+      isCompleted: false
+    }
+]);
+
+return (
+  <div className="app">
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todo} />
+      ))}
+    </div>
+  </div>
+)
+
+
+}
+
+export default App;
